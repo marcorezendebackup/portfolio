e1bd84a09ab2a09a27fdf45f0f323b8fb9edea27
Marco <marcorezende@outlook.com.br>
2019-09-24 22:29:56 -0300

    Updates

---
+++ precache-manifest.a40ac89b948e8c6cb657d90399192453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99606a0061529cb70189f62e36719b69",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d28e42e0bcce9f45999e",
+    "url": "/portfolio/static/css/2.15189a38.chunk.css"
+  },
+  {
+    "revision": "b834bdf70830114d44fe",
+    "url": "/portfolio/static/css/main.52d551e7.chunk.css"
+  },
+  {
+    "revision": "d28e42e0bcce9f45999e",
+    "url": "/portfolio/static/js/2.e1f64ea0.chunk.js"
+  },
+  {
+    "revision": "b834bdf70830114d44fe",
+    "url": "/portfolio/static/js/main.203bdad2.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  },
+  {
+    "revision": "3e545f7ba640c5435941b49a54d2d7d5",
+    "url": "/portfolio/static/media/ex.3e545f7b.jpg"
+  }
+]);
