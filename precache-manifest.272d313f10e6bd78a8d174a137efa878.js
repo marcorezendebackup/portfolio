bc9e7d6e372ac17e7363faa8b203e80815ef4058
Marco <marcorezende@outlook.com.br>
2019-12-10 18:45:59 -0300

    Updates

--- precache-manifest.272d313f10e6bd78a8d174a137efa878.js
+++ precache-manifest.272d313f10e6bd78a8d174a137efa878.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "708fc7af3cfe4d64582b9e752069887e",
+    "revision": "f61978d4cd5a544d31d547b5d73a5be8",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.246caf7e.chunk.css"
   },
   {
-    "revision": "0e366dae4434d13bb862",
-    "url": "/portfolio/static/css/main.32b1068b.chunk.css"
+    "revision": "316e9fd747cc3f986b26",
+    "url": "/portfolio/static/css/main.c7239bc3.chunk.css"
   },
   {
     "revision": "771059a22ca49fbadd31",
     "url": "/portfolio/static/js/2.ea0c23c5.chunk.js"
   },
   {
-    "revision": "0e366dae4434d13bb862",
-    "url": "/portfolio/static/js/main.03244825.chunk.js"
+    "revision": "316e9fd747cc3f986b26",
+    "url": "/portfolio/static/js/main.3be5c876.chunk.js"
   },
   {
     "revision": "5029282666060c428c88",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5babc5d73f0b6e98e5f200061f06f1a7.js"
+  "/portfolio/precache-manifest.272d313f10e6bd78a8d174a137efa878.js"
 );
 
 self.addEventListener('message', (event) => {
