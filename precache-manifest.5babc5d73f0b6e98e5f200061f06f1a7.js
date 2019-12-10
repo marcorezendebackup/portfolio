89e560d36367252ffc04752ebaf23ce6cad57977
Marco <marcorezende@outlook.com.br>
2019-12-10 00:54:34 -0300

    Updates

---
+++ precache-manifest.5babc5d73f0b6e98e5f200061f06f1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708fc7af3cfe4d64582b9e752069887e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "771059a22ca49fbadd31",
+    "url": "/portfolio/static/css/2.246caf7e.chunk.css"
+  },
+  {
+    "revision": "0e366dae4434d13bb862",
+    "url": "/portfolio/static/css/main.32b1068b.chunk.css"
+  },
+  {
+    "revision": "771059a22ca49fbadd31",
+    "url": "/portfolio/static/js/2.ea0c23c5.chunk.js"
+  },
+  {
+    "revision": "0e366dae4434d13bb862",
+    "url": "/portfolio/static/js/main.03244825.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.49d0e8a3efcb2d14c89a69472c2effbe.js"
+  "/portfolio/precache-manifest.5babc5d73f0b6e98e5f200061f06f1a7.js"
 );
 
 self.addEventListener('message', (event) => {
